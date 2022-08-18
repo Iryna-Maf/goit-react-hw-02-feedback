82044d75fcea2adc84366aa609a93e0fd6a122a8
Iryna-Maf <100190659+Iryna-Maf@users.noreply.github.com>
2022-08-18 14:13:31 +0300

    done statistics

---
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+import s from '../FeedbackOptions/FeedbackOptions.module.css';
+import { nanoid } from 'nanoid';
+
+const FeedbackOptions = ({ setFeedback, options }) => {
+  const buttons = options.map(item => (
+    <button
+      className={s.btn}
+      key={nanoid()}
+      type="button"
+      name={item}
+      onClick={setFeedback}
+    >
+      {item}
+    </button>
+  ));
+  return <div className={s.btnList}>{buttons}</div>;
+};
+export default FeedbackOptions;
+
+FeedbackOptions.propTypes = {
+  setFeedback: PropTypes.func.isRequired,
+  options: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+  }),
+};
